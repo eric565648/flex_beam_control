64e3cc23a93a1c95ca96e9685d77256d5e037558
Eric Lu <eric565648@gmail.com>
2022-12-07 15:24:40 -0500

    falcon space control

--- dev_ws/src/kin_control/src/joystick_space_control.py
+++ dev_ws/src/kin_control/src/joystick_space_control.py
@@ -4,7 +4,7 @@ import rospy
 import rospkg
 import numpy as np
 from sensor_msgs.msg import Joy,JointState
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64MultiArray,Int32
 
 ### robotics
 from general_robotics_toolbox import *
@@ -25,7 +25,17 @@ HOME_P=1
 HOME_I=0.1
 HOME_D=0.01
 
-class Joy2Vel(object):
+### space mapping
+falcon_x=[-40,40]
+robot_x=[450,800]
+falcon_y=[-40,40]
+robot_y=[100,450]
+potential_range=800
+robot_theta_range=np.radians(90)
+robot_theta_upper=np.radians(45)
+robot_theta_lower=np.radians(-45)
+
+class Joy2Space(object):
     
     def __init__(self) -> None:
 
@@ -33,49 +43,70 @@ class Joy2Vel(object):
         self.rospack =rospkg.RosPack()
         self.robot=robot_obj(self.rospack.get_path('kin_control')+'/config/UR5e_robot_default_config.yml')
         self.robot_q = None
+        self.HOME_POS_CART=self.robot.fwd(HOME_POS).p*1000
 
-        self.use_falcon=False
-        if rospy.has_param('use_falcon'):
-            self.use_falcon=rospy.get_param('use_falcon')
+        self.use_falcon=True
         
         self.joint_vel_pub = rospy.Publisher('/joint_group_vel_controller/command',
         Float64MultiArray,queue_size=1)  
         self.vel_pub = rospy.Publisher('vel_cmd',Float64MultiArray,queue_size=1)    
 
-        self.omega_th0 = 0
-        self.vel_x = 0
-        self.vel_y = 0
+        self.target = np.array([0,0])
         if not self.use_falcon:
             self.joy_topic_name='/joy'
         else:
             self.joy_topic_name='/falcon_joy'
         
-        self.sub_joy = rospy.Subscriber(self.joy_topic_name,Joy,self.joy_cb,queue_size=1)
+        self.sub_joy = rospy.Subscriber(self.joy_topic_name,JointState,self.joy_cb,queue_size=1)
+        self.sub_knob= rospy.Subscriber('joy_knob',Int32)
         self.sub_states = rospy.Subscriber('/joint_states',JointState,self.joint_state_cb,
                             queue_size=1)
 
         ### go home button
         self.go_home_flag=False
 
+        ### anchoring angle position
+        self.anchor_angle=False
+        self.theta_scale=robot_theta_range/potential_range
+        self.knob_theta_start=None
+        self.robot_theta_start=None
+        self.target_theta=None
+    
+    def knob_cb(self,msg):
+
+        if self.anchor_angle:
+            self.knob_theta_start=msg.data
+            self.robot_theta_start=self.robot_q[-1]
+            print("Reset Knob Theta")
+            self.anchor_angle=False
+
+        if self.knob_theta_start is not None:
+            self.target_theta=(msg.data-self.knob_theta_start)*self.theta_scale+self.robot_theta_start
+
     def joy_cb(self,msg):
+        # self.go_home_flag=True
 
         if not self.use_falcon:
-            self.omega_th0 = msg.axes[3]*MAX_OMG if np.fabs(msg.axes[3])>=DEAD_ZONE else 0
-            # self.vel_x = msg.axes[-2]*-1*MAX_VEL if np.fabs(msg.axes[-2])>=DEAD_ZONE else 0
-            # self.vel_y = msg.axes[-1]*MAX_VEL if np.fabs(msg.axes[-1])>=DEAD_ZONE else 0
-            self.vel_x = msg.axes[0]*-1*MAX_VEL if np.fabs(msg.axes[0])>=DEAD_ZONE else 0
-            self.vel_y = msg.axes[1]*MAX_VEL if np.fabs(msg.axes[1])>=DEAD_ZONE else 0
+            return
         else:
-            self.omega_th0 = msg.axes[2]*MAX_OMG if np.fabs(msg.axes[2])>=DEAD_ZONE else 0
-            self.vel_x = msg.axes[0]*MAX_VEL if np.fabs(msg.axes[0])>=DEAD_ZONE else 0
-            self.vel_y = msg.axes[1]*MAX_VEL if np.fabs(msg.axes[1])>=DEAD_ZONE else 0
+            self.target_x=((msg.axes[0]-falcon_x[0])/(falcon_x[0]-falcon_x[1]))*(robot_x[0]-robot_x[1])+robot_x[0]
+            self.target_y=((msg.axes[1]-falcon_y[0])/(falcon_y[0]-falcon_y[1]))*(robot_y[0]-robot_y[1])+robot_y[0]
 
         if msg.buttons[2] == 1:
             self.go_home_flag=True
         else:
             self.go_home_flag=False
+        
+        if msg.buttons[3] == 0:
+            self.b3_release=True
+        if msg.buttons[3] == 1 and self.b3_release:
+            self.anchor_angle=True
+            self.b3_release=False
 
     def joint_state_cb(self,msg):
+
+        if not self.use_falcon:
+            return
 
         self.robot_q=np.array(msg.position)
         self.robot_q=self.robot_q[COMPENSET]
@@ -88,32 +119,50 @@ class Joy2Vel(object):
             return
 
         robot_J = self.robot.jacobian(self.robot_q)
-        # print(robot_J)
-        vx=self.vel_x
-        vy=self.vel_y
-        ### inner hard constraints
-        robot_p=self.robot.fwd(self.robot_q).p*1000
-        if robot_p[1]>=INNER_CONS:
-            if vx<0:
-                vx=0
+        
+        p_d = np.array([0,0,0,self.HOME_POS_CART[0],X_COMPENSET*self.target_x,Y_COMPENSET*self.target_y])
+        # print(p_d)
+        p_t = np.append([0,0,0],self.robot.fwd(self.robot_q).p*1000)
+        # p_t[3]=0
+        # print(p_t)
+
+        Kp=5
+        if rospy.has_param('Kp_space'):
+            Kp=rospy.get_param('Kp_space')
+        vd=Kp*(p_d-p_t)
+        if p_t[4]>=INNER_CONS:
+            if vd[4]>0:
+                vd[4]=0
+        vd_constrain=2000
+        vd_stop=vd_constrain*2
+        if np.linalg.norm(vd)>vd_constrain:
+            vd=vd/np.linalg.norm(vd)*vd_constrain
+        if np.linalg.norm(vd)>vd_stop:
+            vd=0
+        vd*=1e-3
+        
         ### outer hard constraints
-        joint_vel = np.matmul(np.linalg.pinv(robot_J),[0,0,0,0,X_COMPENSET*vx,Y_COMPENSET*vy])
+        joint_vel = np.matmul(np.linalg.pinv(robot_J),vd)
         # print(self.robot_q[2])
         if self.robot_q[2]>OUTER_CONS:
             if joint_vel[2]>0:
                 joint_vel=np.zeros(joint_vel.shape)
         # print(joint_vel)
         
-        joint_vel[5] += self.omega_th0*MAX_OMG
-        # print(vx,vy)
-        # print(joint_vel)
+        if self.target_theta is not None:
+            Kp_theta=5
+            joint_vel[5] =Kp_theta*(self.target_theta-self.robot_q[-1])
+            if self.robot_q<robot_theta_lower:
+                joint_vel[5]=max([0,joint_vel[5]])
+            if self.robot_q>robot_theta_lower:
+                joint_vel[5]=min([0,joint_vel[5]])
 
         vel_msg = Float64MultiArray()
         vel_msg.data=joint_vel
         # print(joint_vel)
         self.joint_vel_pub.publish(vel_msg)
         v_msg=Float64MultiArray()
-        v_msg.data=[vx,vy,self.omega_th0*MAX_OMG]
+        v_msg.data=[X_COMPENSET*vd[4],Y_COMPENSET*vd[5],joint_vel[5]]
         self.vel_pub.publish(v_msg)
     
     def go_home(self):
@@ -130,6 +179,6 @@ class Joy2Vel(object):
 
 if __name__=='__main__':
     
-    rospy.init_node('joy_to_vel')
-    j2v=Joy2Vel()
+    rospy.init_node('joy_to_space')
+    j2s=Joy2Space()
     rospy.spin()
--- dev_ws/src/kin_control/src/motion_range.py
+++ dev_ws/src/kin_control/src/motion_range.py
@@ -17,8 +17,8 @@ from ur5_robot_def import *
 res=1
 x_range=np.arange(-70,70,res)
 y_range=np.arange(-70,70,res)
-x_range=np.arange(150,1200,res)
-y_range=np.arange(-300,750,res)
+# x_range=np.arange(150,1200,res)
+# y_range=np.arange(-300,750,res)
 motion_plot=np.zeros((len(y_range),len(x_range)))
 pos=None
 
@@ -34,6 +34,14 @@ y_min=9999999
 z_max=-9999999
 z_min=9999999
 
+### robot range
+# x: 455, 888  (range: 433)
+# y: -346, 871 (rabge: 1217, half: 262.5)
+
+### falcon range
+# x: -55 59 (range 114)
+# y: -56 56 (range 112)
+
 def animate(i):
 
     ax.set_xticks=np.arange(0,70,10)
@@ -45,22 +53,34 @@ def animate(i):
 
 def joint_cb(msg):
 
+    global pos,y_min,y_max,z_min,z_max
+
     ## falcon plot
-    # x_index = np.argmax(x_range>msg.position[0])-1
-    # y_index = np.argmax(y_range>msg.position[1])
-    # global pos
-    # pos=np.round(msg.position,1)
-    # motion_plot[-y_index,x_index]=1
+    x_index = np.argmax(x_range>msg.position[0])-1
+    y_index = np.argmax(y_range>msg.position[1])
+    global pos
+    pos=np.round(msg.position,1)
+    if pos[0]<y_min:
+        y_min=pos[0]
+    if pos[0]>y_max:
+        y_max=pos[0]
+    if pos[1]<z_min:
+        z_min=pos[1]
+    if pos[1]>z_max:
+        z_max=pos[1]
+    # print(y_min,y_max)
+    # print(z_min,z_max)
+    # print(msg.position)
+    motion_plot[-y_index,x_index]=1
     
     ## robot plot
-    robot_q=np.array(msg.position)
-    robot_q=robot_q[COMPENSET]
-    robot_p=robot.fwd(robot_q).p*1000
-    global pos,y_min,y_max,z_min,z_max
-    pos=np.round(robot_p)
-    x_index = np.argmax(x_range>-pos[1])-1
-    y_index = np.argmax(y_range>pos[2])
-    # print(pos)
+    # robot_q=np.array(msg.position)
+    # robot_q=robot_q[COMPENSET]
+    # robot_p=robot.fwd(robot_q).p*1000
+    # pos=np.round(robot_p)
+    # x_index = np.argmax(x_range>-pos[1])-1
+    # y_index = np.argmax(y_range>pos[2])
+    # # print(pos)
     # if pos[1]<y_min:
     #     y_min=pos[1]
     # if pos[1]>y_max:
@@ -71,16 +91,16 @@ def joint_cb(msg):
     #     z_max=pos[2]
     # print(y_min,y_max)
     # print(z_min,z_max)
-    print(pos)
-    motion_plot[-y_index,x_index]=1
+    # print(pos)
+    # motion_plot[-y_index,x_index]=1
     
 
 if __name__=='__main__':
 
     rospy.init_node("Motion_Range")
 
-    # sub_joint = rospy.Subscriber('falcon_cart_state',JointState,joint_cb,queue_size=1)
-    sub_joint = rospy.Subscriber('joint_states',JointState,joint_cb,queue_size=1)
+    sub_joint = rospy.Subscriber('falcon_cart_state',JointState,joint_cb,queue_size=1)
+    # sub_joint = rospy.Subscriber('joint_states',JointState,joint_cb,queue_size=1)
     
     ani=animation.FuncAnimation(fig,animate,interval=10)
     plt.show()
